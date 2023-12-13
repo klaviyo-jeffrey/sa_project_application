2e05cf13d308012a9b79857f79255070d1e20a8a
Jeffrey Vaz <97193911+klaviyo-jeffrey@users.noreply.github.com>
2023-12-12 17:09:44 -0800

    write conversion data to csv file

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const fetch = require('node-fetch');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+// https://www.npmjs.com/package/csv-writer
+const fs = require('fs');
 
 const API_KEY = process.env.PRIV_API_KEY || require('./config').PRIV_API_KEY;
 
@@ -31,12 +34,11 @@ fetch(url, {
     // loop through campaignIds array to make ajax call for conversions
     for (let i = 0; i < campaignIds.length; i++) {
       let ajaxUrl = `https://www.klaviyo.com/ajax/campaign/${campaignIds[i]}/reports/conversions?dimension=Name&statistic=${orderMetricId}`
-      // let ajaxUrl = `https://www.klaviyo.com/ajax/campaign/01HGEZVYDWJV92R4XR307YGGQ1/reports/conversions?dimension=Name&statistic=Y3c8te`
       const ajaxHeaders = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         'X-Csrftoken': 'Gklf8Ob2dYVtWSPYttP9S7Rti5rkTmKBfA7hZNyK0tP525dIKOBAsIbs6XMfXvXS',
-        'Cookie': '__kla_id=eyJjaWQiOiJPRFV5T1Rka016SXROR1JpTkMwMFltUmtMVGcxT0RBdE1ETm1OR0ZoWTJRNU5UVXciLCIkZmlyc3RfbmFtZSI6IkplZmZyZXkiLCIkbGFzdF9uYW1lIjoiVmF6IElJIiwiSXNTdGFmZiI6dHJ1ZSwiQWNjb3VudElEIjoiOUJYM3doIiwiY3VycmVudF9jb21wYW55X2lkIjoiOUJYM3doIiwiJGV4Y2hhbmdlX2lkIjoiTWM2bDdMREFWMDZmOTVTeVpjRUxIR3R4TDZLeU1XQ0h0anlZb3VGbWdVST0uOUJYM3doIiwiJHJlZmVycmVyIjp7InRzIjoxNjg0NTM4NDI5LCJ2YWx1ZSI6Imh0dHBzOi8vd3d3LmtsYXZpeW8uY29tL3N0YWZmL2FjY291bnQvTkpEVFdzL2ludGVncmF0aW9ucyIsImZpcnN0X3BhZ2UiOiJodHRwczovL3d3dy5rbGF2aXlvLmNvbS9zdGFmZi9hY2NvdW50L05KRFRXcy9pbnRlZ3JhdGlvbi8xNDkxNjM5In0sIiRsYXN0X3JlZmVycmVyIjp7InRzIjoxNzAxOTk2Nzc3LCJ2YWx1ZSI6IiIsImZpcnN0X3BhZ2UiOiJodHRwczovL3d3dy5rbGF2aXlvLmNvbS9wcm9maWxlLzAxSEgzRTk5NVc2MkU0OFo2RFlBQlBWQlIxIn19; __kla_off=1; _uetsid=ac9c0aa0986e11ee9af9a3a3a7f317eb; _uetvid=29c96820e9dd11edb2b11b1bf62450ea; _gat=1; kl_csrftoken=Gklf8Ob2dYVtWSPYttP9S7Rti5rkTmKBfA7hZNyK0tP525dIKOBAsIbs6XMfXvXS; kl_sessionid=c16epwve28827c3czll2j41h3sxq3ld4;'
+        'Cookie': '__kla_id=eyJjaWQiOiJPRFV5T1Rka016SXROR1JpTkMwMFltUmtMVGcxT0RBdE1ETm1OR0ZoWTJRNU5UVXciLCIkZmlyc3RfbmFtZSI6IkplZmZyZXkiLCIkbGFzdF9uYW1lIjoiVmF6IElJIiwiSXNTdGFmZiI6dHJ1ZSwiQWNjb3VudElEIjoiOUJYM3doIiwiY3VycmVudF9jb21wYW55X2lkIjoiOUJYM3doIiwiJGV4Y2hhbmdlX2lkIjoiTWM2bDdMREFWMDZmOTVTeVpjRUxIR3R4TDZLeU1XQ0h0anlZb3VGbWdVST0uOUJYM3doIiwiJHJlZmVycmVyIjp7InRzIjoxNjg0NTM4NDI5LCJ2YWx1ZSI6Imh0dHBzOi8vd3d3LmtsYXZpeW8uY29tL3N0YWZmL2FjY291bnQvTkpEVFdzL2ludGVncmF0aW9ucyIsImZpcnN0X3BhZ2UiOiJodHRwczovL3d3dy5rbGF2aXlvLmNvbS9zdGFmZi9hY2NvdW50L05KRFRXcy9pbnRlZ3JhdGlvbi8xNDkxNjM5In0sIiRsYXN0X3JlZmVycmVyIjp7InRzIjoxNzAxOTk2Nzc3LCJ2YWx1ZSI6IiIsImZpcnN0X3BhZ2UiOiJodHRwczovL3d3dy5rbGF2aXlvLmNvbS9wcm9maWxlLzAxSEgzRTk5NVc2MkU0OFo2RFlBQlBWQlIxIn19; __kla_off=1; _uetsid=ac9c0aa0986e11ee9af9a3a3a7f317eb; _uetvid=29c96820e9dd11edb2b11b1bf62450ea; _gat=1; kl_csrftoken=rwHlPFpKwIMrFkae2Kmuf2MVZpUXoSPHgaVi8TlXBi4lna6kh1pEfDwx4XfuLTqS; kl_sessionid=mkgo6m0jrzdpnjf7q1ggpruu11gginwm;'
       }
       fetch(ajaxUrl, {
         method: 'GET',
@@ -52,9 +54,37 @@ fetch(url, {
           }
         })
         .then(data => {
-          // console.log(data);
           let sortedData = data.sort((a, b) => b.uniques - a.uniques);
+          let currentCampaignId = campaignIds[i];
           console.log(sortedData);
+          const csvWriter = createCsvWriter({
+            path: '/Users/jeffrey.vaz/scratch-projects/sa_project_application/records.csv',
+            header: [
+              { id: 'campaign', title: 'Campaign ID' },
+              { id: 'name', title: 'Item Name' },
+              { id: 'uniques', title: 'Unique Purchasers' },
+              { id: 'value', title: 'Total Value' },
+              { id: 'count', title: 'Total Count' },
+            ],
+          });
+
+          const records = [];
+          for (let j = 0; j < sortedData.length; j++) {
+          
+            records.push({
+              campaign: currentCampaignId,
+              name: sortedData[j].name,
+              uniques: sortedData[j].uniques,
+              value: sortedData[j].value,
+              count: sortedData[j].count,              
+            });
+          }
+          console.log('logging records');
+          console.log(records);
+          csvWriter.writeRecords(records)
+          .then(() => console.log('CSV file writing complete'))
+          .catch(error => console.error('CSV file writing error', error));
+
         })
         .catch(error => {
           console.error('Error:', error);
